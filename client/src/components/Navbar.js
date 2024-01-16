e39c1663e8e2be5b128367d849fc3d5d9f512c6a
Mattia <mattione7@gmail.com>
2024-01-16 23:34:43 +0000

    solved search bar bug and added basket and settings buttons

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -12,6 +12,9 @@ function Navbar({runQuery}) {
     runQuery(userSearch);
   }
 
+  const basketClick = (event) => {}
+  const settingsClick = (event) => {}
+
   return (
     <nav className="flex bg-indigo-950 overflow-hidden">
       <img src={toucan} alt="Logo" className='float-left p-2 ml-4 w-28'/>
@@ -23,15 +26,18 @@ function Navbar({runQuery}) {
           placeholder="Search products..."
           value={userSearch}
           onChange={(e) => setUserSearch(e.target.value)}
-          
+          className=" bg-indigo-400 self-center placeholder-slate-950 outline-none"
         />
         <button onClick={handleButtonClick} className="ml-auto self-center p-4 my-4 bg-indigo-400 text-slate-950 rounded h-16 w-28">Search</button>
       </form>
 
 
-
-      <img src={basket} alt="Logo" className='float-right self-center p-2 m-2 mx-2 w-16 h-full'/>
-      <img src={settings} alt="Logo" className='float-right self-center p-2 m-2 mx-2 w-14 h-full'/>
+      <button className='float-right self-center p-2 m-2 mx-2 w-24 h-full'>
+        <img src={basket} onClick={basketClick}/>
+      </button>
+      <button className='float-right self-center p-2 m-2 mx-2 w-20 h-full'>
+        <img src={settings} onClick={settingsClick}/>
+      </button>
     </nav>
   );
 };
