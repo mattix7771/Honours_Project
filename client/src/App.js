3c7daad2ae50924c0cdf8d490914e44cf81bb600
Mattia <mattione7@gmail.com>
2024-01-17 15:50:21 +0000

    Working router

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Basket from './pages/Basket';
 import Settings from './pages/Settings';
@@ -7,14 +7,13 @@ import Settings from './pages/Settings';
 function App() {
 
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
-        <Route path="/" element={<Home />}>
-        <Route path="basket" element={<Basket />} />
-        <Route path="settings" element={<Settings />} />
-        </Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/basket" element={<Basket />} />
+        <Route path="/settings" element={<Settings />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
 import toucan from '../resources/toucan.png'; //Free image from freepik.com
 import settings from '../resources/settings.png'; //Free image from freepik.com
 import basket from '../resources/basket.png'; //Free image from freepik.com
@@ -12,9 +13,6 @@ function Navbar({runQuery}) {
     runQuery(userSearch);
   }
 
-  const basketClick = (event) => {}
-  const settingsClick = (event) => {}
-
   return (
     <nav className="flex bg-indigo-950 overflow-hidden">
       <img src={toucan} alt="Logo" className='float-left p-2 ml-4 w-28'/>
@@ -26,18 +24,21 @@ function Navbar({runQuery}) {
           placeholder="Search products..."
           value={userSearch}
           onChange={(e) => setUserSearch(e.target.value)}
-          className=" bg-indigo-400 self-center placeholder-slate-950 outline-none"
+          className=" bg-indigo-400 self-center placeholder-slate-950 outline-none w-full"
         />
         <button onClick={handleButtonClick} className="ml-auto self-center p-4 my-4 bg-indigo-400 text-slate-950 rounded h-16 w-28">Search</button>
       </form>
 
-
-      <button className='float-right self-center p-2 m-2 mx-2 w-24 h-full'>
-        <img src={basket} onClick={basketClick}/>
-      </button>
-      <button className='float-right self-center p-2 m-2 mx-2 w-20 h-full'>
-        <img src={settings} onClick={settingsClick}/>
-      </button>
+      <Link to='/basket' className='float-right self-center p-2 m-2 mx-2 w-24 h-full'>
+        <button>
+          <img src={basket}/>
+        </button>
+      </Link>
+      <Link to='/settings' className='float-right self-center p-2 m-2 mx-2 w-20 h-full'>
+        <button>
+          <img src={settings}/>
+        </button>
+      </Link>
     </nav>
   );
 };
