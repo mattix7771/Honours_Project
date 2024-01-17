41397bef637fd1115ba90d58428c8d341e4b5377
Mattia <mattione7@gmail.com>
2024-01-17 23:45:57 +0000

    visuals

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -2,26 +2,31 @@ import React from 'react';
 
 function Product(props) {
 
-  const starCount = Math.round(props.rating.split(' ').slice(0,1));
-
-  return (
-    <div className='w-64 h-80 m-4 mb-14'>
-      <div className='bg-blue-50 rounded-3xl flex justify-center items-center p-6 mx-3'>
-        <img src={props.image} className='max-h-[160px] min-h-[160px]'/>
+  
+  if (props.name == undefined || props.price == undefined || props.image == undefined || props.rating == undefined) {
+    return 0;
+  } else{
+    const starCount = Math.round(props.rating.split(' ').slice(0,1));
+    return (
+      <div className='w-64 h-80 m-4 mb-14'>
+        <div className='bg-blue-50 rounded-3xl flex justify-center items-center p-6 mx-3'>
+          <img src={props.image} className='max-h-[160px] min-h-[160px]'/>
+        </div>
+        <div className='px-5 py-2'>
+          <a className='font-bold'>{props.name.split(' ').slice(0, 3).join(' ')}</a><br/>
+          <a>{props.name.split(' ').slice(4, 10).join(' ')}</a><br/>
+          <a>{props.price}</a><br/>
+          <a>
+            {Array(starCount).fill().map((_, index) => (
+              <span key={index}>⭐</span>
+            ))}
+          </a><br/>
+  
+        </div>
       </div>
-      <div className='px-5 py-2'>
-        <a className='font-bold'>{props.name.split(' ').slice(0, 3).join(' ')}</a><br/>
-        <a>{props.name.split(' ').slice(3, 10).join(' ')}</a><br/>
-        <a>{props.price}</a><br/>
-        <a>
-          {Array(starCount).fill().map((_, index) => (
-            <span key={index}>⭐</span>
-          ))}
-        </a><br/>
+    );
 
-      </div>
-    </div>
-  );
+  }
 };
 
 export default Product;
---
+++ client/src/pages/Basket.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Basket() {
+
+  
+
+  return (
+    <>
+        Basket page
+    </>
+  );
+}
+
+export default Basket;
