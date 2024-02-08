3e67bc22fba4973c823a121eff021b71fc6ba9b6
Mattia <mattione7@gmail.com>
2024-02-08 22:26:51 +0000

    Added limit to getSpecific products Added option to search for multiple product names in /tittles

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,29 +1,31 @@
 import express from 'express';
-import { getAllProducts , getAllCategory, getProductsByTitle, getSpecificProduct } from '../database.js';
+import { getAllProducts , getAllFromTable, getProductsByTitle, getSpecificProduct } from '../database.js';
 
 // Product routes
 const router = express.Router();
 
 // Get specific product
-router.get('/getSpecificProduct/:productType/:productFilter/:direction(ASC|DESC)', async (req, res) => {
+router.get('/getSpecificProduct/:productType/:productFilter/:direction(ASC|DESC)/:limit', async (req, res) => {
   const productType = req.params.productType;
   const productFilter = req.params.productFilter;
   const direction = req.params.direction;
-  const products = await getSpecificProduct(productType, productFilter, direction);
+  const limit = req.params.limit;
+  const products = await getSpecificProduct(productType, productFilter, direction, limit);
   res.send(products);
 });
 
 // Get products by title
-router.get('/:title', async (req, res) => {
-  const title = req.params.title;
-  const products = await getProductsByTitle(title);
+router.get('/:titles', async (req, res) => {
+  const titles = req.params.titles.split(',');
+  const products = await getProductsByTitle(titles);
   res.send(products);
 });
+
 
 // Get all products by category
 router.get('/category/:category', async (req, res) => {
   const category = req.params.category;
-  const products = await getAllCategory(category);
+  const products = await getAllFromTable(category);
   res.send(products);
 });
 
